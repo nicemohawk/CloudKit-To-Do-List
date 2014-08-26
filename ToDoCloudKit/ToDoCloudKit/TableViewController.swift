4be0ce858eed852b9689a4bcf90fa7056ccc381b
Ben Lachman <ben@nicemohawk.com>
2014-08-26 10:07:14 -0600

    Pre Demo shell

--- ToDoCloudKit/ToDoCloudKit/TableViewController.swift
+++ ToDoCloudKit/ToDoCloudKit/TableViewController.swift
@@ -16,71 +16,13 @@ class TableViewController: UITableViewController, UITableViewDataSource, UITable
 
 	// Function to delete all todos in the UITableView and database
 	@IBAction func deleteTodos(sender: AnyObject) {
-		// Create the query to load the todos
-
-		todoStore.deleteAllRecordsWithCompletion { (error) -> Void in
-			// Reload the UITableView and retreive the new contents
-			dispatch_async(dispatch_get_main_queue()) {
-				self.tableView.reloadData()
-			}
-		}
 	}
 	
 	// Function to load all todos in the UITableView and database
 	func loadTodos() {
-		// Finish fetching the items for the recordX
-		todoStore.fetchAllRecordsWithCompletion() { (cursor, error) in
-			if error != nil {
-				println(error)
-			}
-
-			// Reload the UITableView with the retreived contents
-			dispatch_async(dispatch_get_main_queue()) {
-				self.tableView.reloadData()
-			}
-		}
 	}
 
 	func completeTodoAtIndexPath(indexPath: NSIndexPath) {
-		// completing a Todo just deletes it.
-		var itemToDelete = todoStore.todos.removeAtIndex(indexPath.row)
-		tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-
-		var database: CKDatabase = CKContainer.defaultContainer().privateCloudDatabase
-
-		database.deleteRecordWithID(itemToDelete.recordID) { (deletedRecord, error) -> Void in
-			if error != nil {
-				println("error: \(error)")
-			} else {
-				println("deleted todo: \(itemToDelete.name)")
-			}
-		}
 	}
 
-	// MARK: UIViewController
-
-	override func viewDidAppear(animated: Bool)  {
-		super.viewDidAppear(animated)
-
-		loadTodos()
-	}
-
-	override func viewDidLoad() {
-		super.viewDidLoad()
-
-		self.tableView.delegate = self
-		self.tableView.dataSource = self
-
-		dispatch_async(dispatch_get_main_queue()) {
-			self.tableView.reloadData()
-		}
-	}
-
-	override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
-		super.prepareForSegue(segue, sender: sender)
-
-		if let newTodoViewController = segue.destinationViewController as? NewTodoViewController {
-			newTodoViewController.todoViewController = self
-		}
-	}
 }
--- ToDoCloudKit/ToDoCloudKit/TodoStore.swift
+++ ToDoCloudKit/ToDoCloudKit/TodoStore.swift
@@ -32,91 +32,14 @@ class TodoStore {
 	}
 
 	func fetchAllRecordsWithCompletion(fetchFinished:((CKQueryCursor!, NSError!) -> Void)!) {
-		var query = CKQuery(recordType: "todo", predicate: NSPredicate(format: "TRUEPREDICATE"))
-		var queryOperation = CKQueryOperation(query: query)
-
-		queryOperation.recordFetchedBlock = { (record: CKRecord!) in
-			self.createOrUpdateTodoForRecord(record)
-		}
-
-		queryOperation.queryCompletionBlock = fetchFinished
-
-		// Create the database where you will retreive your new data from
-		var database: CKDatabase = CKContainer.defaultContainer().privateCloudDatabase
-		database.addOperation(queryOperation)
 	}
 
 	func deleteAllRecordsWithCompletion( deleteFinished: ((NSError!) -> Void)! ) {
-		self.fetchAllRecordsWithCompletion { (cursor, error) in
-			if error != nil {
-				println(error)
-			}
-
-			// Iterate through the array content ids
-			var ids : [CKRecordID] = []
-
-			for todo in self.todos {
-				if let recordID = todo.recordID? {
-					ids.append(recordID)
-				}
-			}
-
-			// retreive the database where we will delete the data from
-			var database: CKDatabase = CKContainer.defaultContainer().privateCloudDatabase
-
-			// Delete the data from the database using the ids we iterated through
-			var deleteItemsOperation: CKModifyRecordsOperation = CKModifyRecordsOperation(recordsToSave: nil, recordIDsToDelete: ids)
-
-			deleteItemsOperation.modifyRecordsCompletionBlock = { (savedRecords, deletedRecordIDs, operationError) -> Void in
-				if deletedRecordIDs.count > 0 {
-					self.todos = []
-				}
-
-				deleteFinished(operationError)
-			}
-			
-			database.addOperation(deleteItemsOperation)
-		}
 	}
 
 	func newTodoWithName(name: String, andPriority priority: String, completion: (Void -> Void)) {
-		// Create record to save todos
-		var record: CKRecord = CKRecord(recordType: "todo")
-		// Save todo description for key: todoNameKey
-		record.setObject(name, forKey: "todoNameKey")
-		// Save priority for key: priorityKey
-		record.setObject(priority, forKey: "priorityKey")
-		// Create the private database for the user to save their data to
-		var database: CKDatabase = CKContainer.defaultContainer().privateCloudDatabase
-
-		// Save data to the database for the record: todo
-		database.saveRecord(record) { (record: CKRecord?, error: NSError?) in
-			if error != nil {
-				println(error)
-			} else {
-				TodoStore.sharedStore.createOrUpdateTodoForRecord(record!)
-			}
-
-			completion()
-		}
 	}
 
 	func createOrUpdateTodoForRecord(record: CKRecord) {
-		var foundTodo: Bool = false
-
-		for (index, todo) in enumerate(todos) {
-			if todo.recordID == record.recordID {
-				foundTodo = true
-
-				self.todos[index].name = record.objectForKey("todoNameKey") as String
-				self.todos[index].priority = record.objectForKey("priorityKey") as String
-
-				break;
-			}
-		}
-
-		if foundTodo == false {
-			todos.append(Todo(record: record))
-		}
 	}
 }
